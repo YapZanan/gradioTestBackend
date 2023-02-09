faf7a1e489e52febadd31c9d957f6b0629af0444
YapZanan <naufalsetiawan64@gmail.com>
2023-02-09 15:42:39 +0700

    add

--- app.py
+++ app.py
@@ -6,65 +6,46 @@ from flask_cors import CORS
 app = Flask(__name__)
 
 model_dict = {}
-CORS(app, origins=["http://localhost:3000"])
+CORS(app)
+
+TASK = "translation"
+CKPT = "facebook/nllb-200-distilled-600M"
+
+model = AutoModelForSeq2SeqLM.from_pretrained(CKPT)
+tokenizer = AutoTokenizer.from_pretrained(CKPT)
+
+device = 0 if torch.cuda.is_available() else -1
 
 
-def load_models():
-    # build model and tokenizer
-    model_name_dict = {
-        'nllb-distilled-600M': 'facebook/nllb-200-distilled-600M',
-        # 'nllb-1.3B': 'facebook/nllb-200-1.3B',
-        # 'nllb-distilled-1.3B': 'facebook/nllb-200-distilled-1.3B',
-        # 'nllb-3.3B': 'facebook/nllb-200-3.3B',
-    }
+def translate_text(text, src_lang, tgt_lang, max_length=400):
+    """
+    Translate the text from source lang to target lang
+    """
+    translation_pipeline = pipeline(TASK,
+                                    model=model,
+                                    tokenizer=tokenizer,
+                                    src_lang=src_lang,
+                                    tgt_lang=tgt_lang,
+                                    max_length=max_length,
+                                    device=device)
 
-    for call_name, real_name in model_name_dict.items():
-        print('\tLoading model: %s' % call_name)
-        model = AutoModelForSeq2SeqLM.from_pretrained(real_name)
-        tokenizer = AutoTokenizer.from_pretrained(real_name)
-        model_dict[call_name+'_model'] = model
-        model_dict[call_name+'_tokenizer'] = tokenizer
-
-
-def translation(source, target, text):
-    source = flores_codes[source]
-    target = flores_codes[target]
-
-    model_name = 'nllb-distilled-600M'
-    model = model_dict[model_name + '_model']
-    tokenizer = model_dict[model_name + '_tokenizer']
-
-    translator = pipeline('translation', model=model, tokenizer=tokenizer, src_lang=source, tgt_lang=target)
-    output = translator(text, max_length=400)
-
-    full_output = output
-    output = output[0]['translation_text']
-    result = {
-        'source': source,
-        'target': target,
-        'result': output,
-        'full_output': full_output
-    }
-    return result
+    result = translation_pipeline(text)
+    return result[0]['translation_text']
 
 
 @app.route("/translate", methods=["POST"])
-def translate():
+def handle_translate():
     data = request.get_json()
     source = data.get("source")
     target = data.get("target")
     text = data.get("text")
     print(source, target, text)
-    result = translation(source, target, text)
+    result = translate_text(text, source, target)
     return jsonify(result)
-
 
 @app.route("/languages", methods=["GET"])
 def getlanguages():
     return jsonify(list(flores_codes.keys()))
 
-
-if __name__ == '__main__':
-    print('\tinit models')
-    load_models()
-    app.run()
+if __name__ == "__main__":
+    app.run(debug=True)
